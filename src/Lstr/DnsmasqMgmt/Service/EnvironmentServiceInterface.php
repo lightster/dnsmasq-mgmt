bbfc8ddaa53b17f90afa3b74181553dac2ecb443
Matt Light <matt.light@illuminateed.com>
2015-08-15 00:46:48 -0700

    Add docblocks to EnvironmentServiceInterface

--- src/Lstr/DnsmasqMgmt/Service/EnvironmentServiceInterface.php
+++ src/Lstr/DnsmasqMgmt/Service/EnvironmentServiceInterface.php
@@ -4,7 +4,27 @@ namespace Lstr\DnsmasqMgmt\Service;
 
 interface EnvironmentServiceInterface
 {
+    /**
+     * Runs all of the commands that need to be
+     * ran to clear the DNS cache.
+     *
+     * @return void
+     */
     public function clearDnsCache();
+
+    /**
+     * Returns all commands that need to be ran
+     * to clear the DNS cache.
+     *
+     * @return array
+     */
     public function getClearCacheCommands();
+
+    /**
+     * Installs dnsmasq and does the initial
+     * dnsmasq configuration.
+     *
+     * @return void
+     */
     public function setupDnsmasq();
 }
